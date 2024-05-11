2c441dc5e14d249066d85ec3bcd8fe7be4e9d4d6
Peacebern <peace8282bern@gmail.com>
2024-05-11 15:27:50 +0100

    app.js comments deleted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import "./style/style.css";
 import ResetPassword from "./pages/ResetPassword";
 import NavBar from "./components/NavBar";
-// import SearchBar from "./components/Search.jsx"
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <div className="bg-mobile-bg">
         <NavBar />
         <ResetPassword />
-        {/* <SearchBar /> */}
       </div>
     </div>
   );
