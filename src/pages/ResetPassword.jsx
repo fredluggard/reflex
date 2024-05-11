1c11424fbe57ad7e728401b6f0b02b34ba4e6257
fredluggard <fredrick.ebuka33@gmail.com>
2024-05-11 17:33:54 +0100

    Complete Mobile view of the onboarding pages

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -26,13 +26,13 @@ function ResetPassword() {
     e.preventDefault();
   };
   return (
-    <div className="p-4 py-6 bg-[#960E20]">
+    <div className="p-4 py-6 bg-[#FFFFFF]">
       <nav className="hidden md:block">
         <NavBar />
       </nav>
       {getCode === "one" ? (
         <div className="h-screen">
-          <div className="flex flex-col justify-start text-white">
+          <div className="flex flex-col justify-start text-[#971B22]">
             <h1 className="font-bold text-2xl">Forgot Password?</h1>
             <p className="text-sm my-6">
               Enter your email address, and we will send you a 6-digit code to
@@ -41,22 +41,19 @@ function ResetPassword() {
           </div>
           <div>
             <form
-              className="flex flex-col justify-start text-white"
+              className="flex flex-col justify-start text-[#971B22]"
               action=""
               method="post"
             >
-              <label className="my-1 text-md" htmlFor="email">
-                Email address
-              </label>
               <input
-                className="h-10 text-md px-3 outline-white w-full text-black rounded-2xl border-2 border-white"
+                className="h-10 text-md px-3 outline-[#971B22] w-full text-black rounded-2xl border-[1px] border-[#971B22]"
                 type="email"
                 id="email"
                 name="email"
-                placeholder="Enter email"
+                placeholder="Email"
               />
               <input
-                className="h-10 text-md px-2 w-full rounded-2xl mt-4 mb-2 bg-[#6B6B6B] border-1 border-white"
+                className="h-10 text-md px-2 w-full rounded-2xl my-5 text-white bg-[#971B22] border-[1px] border-[#971B22]"
                 type="submit"
                 value="Send code"
                 onClick={() => SetGetCode("two")}
@@ -64,7 +61,7 @@ function ResetPassword() {
               <div className="flex gap-1">
                 <p className="text-sm">Remember password? </p>
                 <Link
-                  className="text-sm text-gray-300 hover:text-white text-opacity-70"
+                  className="text-sm text-[#971B22] hover:text-gray-800 text-opacity-80"
                   to="/login"
                 >
                   login
@@ -74,8 +71,8 @@ function ResetPassword() {
           </div>
         </div>
       ) : getCode === "two" ? (
-        <div className="h-screen">
-          <div className="flex flex-col justify-start text-white">
+        <div className="h-screen text-[#971B22] bg-[#FFFFFF]">
+          <div className="flex flex-col justify-start">
             <h1 className="font-bold text-2xl">Forgot Password?</h1>
             <p className="text-sm my-6">
               The recovery code was sent to your email address. please enter the
@@ -84,50 +81,50 @@ function ResetPassword() {
           </div>
           <div>
             <form
-              className="flex flex-col justify-start text-white"
+              className="flex flex-col justify-start"
               action=""
               method="GET"
             >
               <div className="flex flex-row gap-2 justify-center">
                 <input
-                  className="h-10 w-10 text-md px-3 outline-white text-black rounded-md border-2 border-gray-400"
+                  className="h-10 w-10 text-md px-3 outline-[#971B22] text-black rounded-md border-[1px] border-[#971B22]"
                   type="text"
                   name="num"
                   id="num1"
                 />
                 <input
-                  className="h-10 w-10 text-md px-3 outline-white text-black rounded-md border-2 border-gray-400"
+                  className="h-10 w-10 text-md px-3 outline-[#971B22] text-black rounded-md border-[1px] border-[#971B22]"
                   type="text"
                   name="num"
                   id="num2"
                 />
                 <input
-                  className="h-10 w-10 text-md px-3 outline-white text-black rounded-md border-2 border-gray-400"
+                  className="h-10 w-10 text-md px-3 outline-[#971B22] text-black rounded-md border-[1px] border-[#971B22]"
                   type="text"
                   name="num"
                   id="num3"
                 />
                 <input
-                  className="h-10 w-10 text-md px-3 outline-white text-black rounded-md border-2 border-gray-400"
+                  className="h-10 w-10 text-md px-3 outline-[#971B22] text-black rounded-md border-[1px] border-[#971B22]"
                   type="text"
                   name="num"
                   id="num4"
                 />
                 <input
-                  className="h-10 w-10 text-md px-3 outline-white text-black rounded-md border-2 border-gray-400"
+                  className="h-10 w-10 text-md px-3 outline-[#971B22] text-black rounded-md border-[1px] border-[#971B22]"
                   type="text"
                   name="num"
                   id="num5"
                 />
                 <input
-                  className="h-10 w-10 text-md px-3 outline-white text-black rounded-md border-2 border-gray-400"
+                  className="h-10 w-10 text-md px-3 outline-[#971B22] text-black rounded-md border-[1px] border-[#971B22]"
                   type="text"
                   name="num"
                   id="num6"
                 />
               </div>
               <button
-                className="h-10 text-md px-2 w-full rounded-2xl mt-4 mb-2 bg-[#6B6B6B] border-1 border-white"
+                className="h-10 text-md px-2 w-full rounded-2xl mt-4 mb-2 text-white bg-[#971B22] border-2 border-[#971B22]"
                 onClick={() => SetGetCode("three")}
               >
                 Recover
@@ -135,8 +132,8 @@ function ResetPassword() {
               <div className="flex gap-1">
                 <p className="text-sm">Didn't receive the code? </p>
                 <Link
-                  className="text-sm text-gray-300 hover:text-white text-opacity-70"
-                  to="/login"
+                  className="text-sm text-[#971B22] hover:text-gray-700 text-opacity-60"
+                  to=""
                 >
                   Resend
                 </Link>
@@ -145,14 +142,14 @@ function ResetPassword() {
           </div>
         </div>
       ) : getCode === "three" ? (
-        <div className="h-screen">
-          <div className="flex flex-col justify-start text-white">
+        <div className="h-screen text-[#971B22] bg-[#FFFFFF]">
+          <div className="flex flex-col justify-start">
             <h1 className="font-bold text-2xl">Reset Password</h1>
             <p className="text-sm my-6">Enter your new password to proceed</p>
           </div>
           <div>
             <form
-              className="flex flex-col justify-start text-white"
+              className="flex flex-col justify-start"
               onSubmit={handleSubmit}
             >
               <label className="my-1 text-md" htmlFor="password">
@@ -160,14 +157,14 @@ function ResetPassword() {
               </label>
               <div className="relative flex">
                 <input
-                  className="h-10 text-md px-3 outline-white w-full text-black rounded-2xl border-2 border-white"
+                  className="h-10 text-md px-3 outline-[#971B22] w-full text-black rounded-2xl border-[1px] border-[#971B22]"
                   type={showPassword ? "text" : "password"}
                   id="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
                 <button
-                  className="text-black absolute right-5 top-3"
+                  className="text-[#971B22] absolute right-5 top-3"
                   type="button"
                   onClick={handleTogglePassword}
                 >
@@ -179,14 +176,14 @@ function ResetPassword() {
               </label>
               <div className="relative flex">
                 <input
-                  className="h-10 text-md px-3 outline-white w-full text-black rounded-2xl border-2 border-white"
+                  className="h-10 text-md px-3 outline-[#971B22] w-full text-black rounded-2xl border-[1px] border-[#971B22]"
                   type={showPassword2 ? "text" : "password"}
                   id="password"
                   value={password2}
                   onChange={(e) => setPassword2(e.target.value)}
                 />
                 <button
-                  className="text-black absolute right-5 top-3"
+                  className="text-[#971B22] absolute right-5 top-3"
                   type="button"
                   onClick={handleTogglePassword2}
                 >
@@ -195,7 +192,7 @@ function ResetPassword() {
               </div>
 
               <input
-                className="h-10 text-md px-2 w-full rounded-2xl mt-4 mb-2 bg-[#6B6B6B] border-1 border-white"
+                className="h-10 text-md px-2 w-full rounded-2xl mt-4 mb-2 text-white bg-[#971B22] border-2 border-[#971B22]"
                 type="submit"
                 value="Reset password"
                 onClick={() => SetGetCode("four")}
@@ -204,17 +201,17 @@ function ResetPassword() {
           </div>
         </div>
       ) : (
-        <div className="flex flex-col h-screen justify-between text-white">
+        <div className="flex flex-col h-screen justify-between bg-[#FFFFFF] text-[#971B22]">
           <div className="flex flex-col justify-center items-center">
             <FaRegCircleCheck className="h-[150px] w-[150px] my-6" />
             <h1 className="font-bold text-2xl">Password Reset Successful</h1>
-            <p className="text-md my-6 opacity-60 text-center">
+            <p className="text-md font-semibold my-6 opacity-60 text-center">
               You have successfully reset your password, Please log into your
               account with new password and email address
             </p>
           </div>
 
-          <button className="h-10 text-md px-2 w-full rounded-2xl bg-[#6B6B6B] text-white border-1 border-white">
+          <button className="h-10 text-md px-2 w-full rounded-2xl bg-[#971B22] text-white border-1 border-white">
             <Link to="/login">sign in</Link>
           </button>
         </div>
