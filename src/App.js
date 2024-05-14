8b223f5c6fded2590c3ec15273fb0897efa6be08
fredluggard <fredrick.ebuka33@gmail.com>
2024-05-14 08:16:27 +0100

    Updated pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "./style/style.css";
 import SignUp from "./components/SignUp";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <div className="bg-mobile-bg">
         <SignUp />
       </div>
