19397601395640f5b18f95bc4078cb04c6be0b3e
fredluggard <fredrick.ebuka33@gmail.com>
2024-05-14 13:44:11 +0100

    updateed carousel arrows

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
 
 function ImageCarousel() {
-  const CustomPrevArrow = (props) => {
-    const { onClick } = props;
+  const sliderRef = useRef(null);
+
+  const CustomPrevArrow = ({ onClick }) => {
     return (
       <button
-        className="flex justify-center items-center relative top-[-40px] mt-[20px] h-[70px] w-[120px] transform -translate-y-1/2 z-10 text-white bg-pink-300 bg-opacity-80 px-4 py-2 rounded-xl"
+        className="flex justify-center items-center relative top-[-30px] left-[0] h-[70px] w-[120px] transform -translate-y-1/2 z-10 text-white bg-pink-300 bg-opacity-80 px-4 py-2 rounded-xl"
         onClick={onClick}
       >
         <FaArrowLeftLong size={"50px"} />
@@ -17,11 +18,10 @@ function ImageCarousel() {
     );
   };
 
-  const CustomNextArrow = (props) => {
-    const { onClick } = props;
+  const CustomNextArrow = ({ onClick }) => {
     return (
       <button
-        className="flex justify-center items-center relative top-[-110px] left-[42%] h-[70px] w-[180px] transform -translate-y-1/2 z-10 text-white bg-black bg-opacity-80 px-4 py-2 rounded-xl"
+        className="flex justify-center items-center relative top-[-95px] left-[42%] h-[70px] w-[180px] transform -translate-y-1/2 z-10 text-white bg-black bg-opacity-80 px-4 py-2 rounded-xl"
         onClick={onClick}
       >
         <FaArrowRightLong size={"50px"} />
@@ -37,26 +37,26 @@ function ImageCarousel() {
     speed: 5000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
+    arrows: false, // Disable default arrows
   };
 
   return (
     <div>
-      <Slider {...settings}>
+      <Slider {...settings} ref={sliderRef}>
         <div className="relative">
           <img src="images/carousel-1.png" alt="services" />
-          <CustomPrevArrow />
-          <CustomNextArrow />
+          <CustomPrevArrow onClick={() => sliderRef.current.slickPrev()} />
+          <CustomNextArrow onClick={() => sliderRef.current.slickNext()} />
         </div>
         <div className="relative">
           <img src="images/carousel-2.png" alt="services" />
-          <CustomPrevArrow />
-          <CustomNextArrow />
+          <CustomPrevArrow onClick={() => sliderRef.current.slickPrev()} />
+          <CustomNextArrow onClick={() => sliderRef.current.slickNext()} />
         </div>
         <div className="relative">
           <img src="images/carousel-3.png" alt="services" />
-          <CustomPrevArrow />
-          <CustomNextArrow />
+          <CustomPrevArrow onClick={() => sliderRef.current.slickPrev()} />
+          <CustomNextArrow onClick={() => sliderRef.current.slickNext()} />
         </div>
       </Slider>
     </div>
