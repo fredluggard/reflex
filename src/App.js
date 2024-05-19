e3295e223ed29ab9ea0c61bf8619a764c98c580e
ifyiyiegbu <ifeyinwaiyiegbu@gmail.com>
2024-05-19 12:17:28 +0100

    created and implemented the settings component and the account preference pages while implementing some changes on the NavBar component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./style/style.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
+
 function App() {
   return (
     <div className="App">
       <div className="bg-mobile-bg">
         <NavBar />
+      
         <Footer />
         <div className="hidden w-[50vw] md:block"></div>
       </div>
