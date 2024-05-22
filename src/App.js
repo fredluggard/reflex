51da06fc32d8734b24707196fb6870a4150de4fe
Fredrick Aniebonam <88670476+fredluggard@users.noreply.github.com>
2024-05-22 08:24:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import "./App.css";
 import "./style/style.css";
-import SignUp from "./components/SignUp";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import ImageCarousel from "./components/ImageCarousel";
-
 
 function App() {
   return (
     <div className="App">
-  
-      <div className="bg-mobile-bg">
+      <div className="bg-mobile-bg md:bg-desktop-bg">
         <NavBar />
         <Footer />
-
-        <div className="hidden w-[50vw] md:block">
-          <ImageCarousel />
-        </div>
       </div>
     </div>
   );
-} <SignUp />
+}
 
 export default App;
