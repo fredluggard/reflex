142e6a858687434ea1ec2d710c5eee9b6d55e535
Vima-lee <victormunach@gmail.com>
2024-05-23 10:28:00 +0100

    accepet changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import "./App.css";
 import "./style/style.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
-import ImageCarousel from "./components/ImageCarousel";
-import FirstAid from "./components/FirstAid";
-import FireSafety from "./components/FireSafety";
-import RoadAndDriverSafety from "./components/RoadAndDriverSafety";
-
-=======
->>>>>>> 2ca0edf3da62cef496433a17006b6dcdcc3f670b
 
 
 function App() {
@@ -19,21 +11,7 @@ function App() {
         <NavBar />
       
         <Footer />
-<<<<<<< HEAD
-
-        <div className="hidden w-[50vw] md:block">
-          <ImageCarousel />
-        </div>
-
-        <FirstAid />
-
-        <FireSafety />
-
-        <RoadAndDriverSafety />
-
-=======
         <div className="hidden w-[50vw] md:block"></div>
->>>>>>> 2ca0edf3da62cef496433a17006b6dcdcc3f670b
       </div>
     </div>
   );
