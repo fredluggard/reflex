418b6cec85bdd19bf9c402c7a491175e8afdc54c
ifyiyiegbu <ifeyinwaiyiegbu@gmail.com>
2024-05-26 00:21:56 +0100

    styled the emergency contact form

--- src/components/EmergencyContactsForm.jsx
+++ src/components/EmergencyContactsForm.jsx
@@ -26,58 +26,65 @@ function EmergencyContactsForm({ onNewContact }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mt-4">
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="border p-1"
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="relationship">Relationship</label>
-        <input
-          type="text"
-          id="relationship"
-          name="relationship"
-          value={relationship}
-          onChange={(e) => setRelationship(e.target.value)}
-          className="border p-1"
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="phone">Phone</label>
-        <input
-          type="text"
-          id="phone"
-          name="phone"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          className="border p-1"
-          required
-        />
-      </div>
-      <div>
-        <label>Picture URL</label>
-        <input
-          type="image"
-          alt="Add a picture"
-          value={picture}
-          onChange={(e) => setPicture(e.target.value)}
-          className="border p-1"
-          required
-        />
-      </div>
-      <button type="submit" className="btn btn-primary mt-2">
-        Add Contact
-      </button>
+    <div>
+    <h2 className="flex justify-center mt-8 text-xl font-semibold text-[#DF3442]">Add a new Emergency contact</h2>
+    <div className="flex justify-center items-center h-screen">
+    
+    <form onSubmit={handleSubmit} className="w-screen p-12">
+    <div className=" mb-4">
+    <label htmlFor="name" className="block font-semibold mb-1">Name</label>
+    <input
+    type="text"
+    id="name"
+    name="name"
+    value={name}
+    onChange={(e) => setName(e.target.value)}
+    className="border border-[#DF3442] w-full md:w-1/2 rounded p-2"
+    required
+    />
+    </div>
+    <div className=" mb-4">
+    <label htmlFor="relationship" className="block font-semibold mb-1">Relationship</label>
+    <input
+    type="text"
+    id="relationship"
+    name="relationship"
+    value={relationship}
+    onChange={(e) => setRelationship(e.target.value)}
+    className="border border-[#DF3442] w-full md:w-1/2 rounded p-2"
+    required
+    />
+    </div>
+    <div className=" mb-4">
+    <label htmlFor="phone" className="block font-semibold mb-1">Phone</label>
+    <input
+    type="text"
+    id="phone"
+    name="phone"
+    value={phone}
+    onChange={(e) => setPhone(e.target.value)}
+    className="border border-[#DF3442] w-full md:w-1/2 rounded p-2"
+    required
+    />
+    </div>
+    <div className=" mb-4">
+    <label className="block font-semibold mb-1">Picture URL</label>
+    <input
+    type="image"
+    alt="Add a picture"
+    value={picture}
+    onChange={(e) => setPicture(e.target.value)}
+    className="border border-[#DF3442] rounded p-12"
+    required
+    />
+    </div>
+    <button type="submit" className="btn btn-primary bg-[#DF3442] text-white border rounded-xl px-8 mt-2">
+    Add Contact
+    </button>
     </form>
+    </div>
+          
+    </div>
   );
 }
 
