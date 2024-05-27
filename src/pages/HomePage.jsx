c8a1fb5c7916bde538a4a2d00e9ebc53611240fd
Fredrick Aniebonam <88670476+fredluggard@users.noreply.github.com>
2024-05-27 07:08:52 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -40,8 +40,8 @@ function HomePage() {
   }, [location]);
 
   return (
-    <div className="flex flex-col justify-between relative h-full">
-      <div className="flex flex-col  justify-evenly relative h-full bg-mobile-bg md:bg-desktop-bg bg-cover bg-center">
+    <div className="flex flex-col justify-between relative h-screen">
+      <div className="flex flex-col  justify-between relative h-full bg-mobile-bg md:bg-desktop-bg bg-cover bg-center">
         <nav>
           <NavBar />
         </nav>
